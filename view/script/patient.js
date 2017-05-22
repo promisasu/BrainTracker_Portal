dc587ffed9b4700d4b8523a98a529c0a4d9112af
Dipak Purbey <dpak005@gmail.com>
2017-05-22 16:52:23 -0700

    removed export and deactivate button from survey-detail page

--- view/script/patient.js
+++ view/script/patient.js
@@ -1,27 +1,8 @@
 (function patient () {
     'use strict';
 
-    var isNewPatient = window.location.search;
-    var isNewPatientRegex = /newPatient=true/;
-
     // Makes a copy of window.dates
     var allDatesConfig = Object.create(window.dates);
-
-
-    //calculate y axis
-    // var datasets = window.dates.datasets;
-    // console.log(datasets);
-    // var yAxes = [];
-    // var labels = [];
-    // for (var i = 0; i < datasets.length; i++) {
-    //   var dataSet = datasets[i];
-    //   console.log("Dataset");
-    //   console.log(dataSet);
-    //   yAxes.push();
-    //   labels.push.apply(labels,dataSet.dates);
-    // }
-    // console.log("Y Axes");
-    // console.log(yAxes);
 
     var config = {
         type: 'line',
@@ -63,27 +44,6 @@
 
     var ctx = document.getElementById('complianceChart').getContext('2d');
 
-    function redirect () {
-        window.location = '/';
-    }
-
-    function warningMessage () {
-        alert('patient could not be deactivated');
-    }
-
-    document.getElementById('deactivate-patient')
-    .addEventListener('click', function deactivate () {
-        $.ajax({
-            url: window.location.pathname,
-            type: 'DELETE'
-        })
-        .done(redirect)
-        .fail(warningMessage);
-    });
-
-    if (isNewPatientRegex.test(isNewPatient)) {
-        $('#remember-patient-dialog').modal('show');
-    }
 
     config.data = allDatesConfig;
     new Chart(ctx, config);
